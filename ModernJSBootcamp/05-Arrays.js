8453501110d0163dad46bf61b799fada39ccdbcc
Yohanes Damenta <gnitnigdy@gmail.com>
2025-01-10 08:36:59 +0700

    includes section in array

--- ModernJSBootcamp/05-Arrays.js
+++ ModernJSBootcamp/05-Arrays.js
@@ -19,11 +19,27 @@ strArrToTest[0] = "h";
 console.log(strArrToTest); // changing
 
 //adding and removing elements in arrays
-lotto.push(9);
+lotto.push(9, 15, 16, 17);
 console.log(lotto);
 lotto.pop();
 console.log(lotto);
-lotto.unshift(10);
+lotto.unshift(10, 11, 12);
 console.log(lotto);
 lotto.shift();
 console.log(lotto);
+
+//array concats -> merging two array
+let fruits = ["apple", "banana", "strawberry"];
+let counts = [1, 2, 3];
+
+let fruitCounts = fruits.concat(counts).concat(strArrToTest);
+console.log(fruitCounts);
+
+counts.pop();
+
+console.log(fruitCounts);
+console.log(counts);
+
+//includes => check if the array have elements that passed in parameter, have to match with element
+console.log(fruitCounts.includes("apple")); //will return true
+console.log(fruitCounts.includes(1)); //will return true
