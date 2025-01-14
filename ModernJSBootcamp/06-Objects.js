a19bbb608bc3a8af6b8d6f3426cdba69d2a796d1
Yohanes Damenta <gnitnigdy@gmail.com>
2025-01-14 07:50:16 +0700

    first half of section 6 object

---
+++ ModernJSBootcamp/06-Objects.js
@@ -0,0 +1,10 @@
+//Object => for collecting data that have not to be in ordered sequence
+// Object is colletions of properties that have key and value pairs.
+// Object sometimes refer to dictionaries
+const fitBitData = {
+  steps: 308756,
+  totalFloor: 1814,
+  longestSteps: 211,
+};
+
+console.log(fitBitData);
