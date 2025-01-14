921bde79a5f6d17b268e56e5b7a87c1b838edd2e
Yohanes Damenta <gnitnigdy@gmail.com>
2025-01-14 22:27:35 +0700

    finishing chapter 6 - object

--- ModernJSBootcamp/06-Objects.js
+++ ModernJSBootcamp/06-Objects.js
@@ -8,3 +8,32 @@ const fitBitData = {
 };
 
 console.log(fitBitData);
+
+//accessing object properties
+const pallete = {
+  red: "#RRR",
+  666: "blue",
+  "sheila on 7": "band",
+  liquor: { liquorName: "whiskey" },
+};
+
+console.log(pallete.red);
+console.log(pallete["666"]);
+console.log(pallete["sheila on 7"]);
+
+//if you access something there is not in object it will return undefined
+
+//adding & updating properties in object
+pallete.walks = "okay";
+pallete["makan nasi"] = "ngga ah";
+pallete["coba duren"] = 1;
+
+pallete["coba duren"] = 1 + 5;
+
+console.log(pallete.liquor["liquorName"]);
+console.log(pallete.liquor.liquorName);
+
+//Objects and reference type -> work same way with array
+//use const for objects
+
+//array and objects equality
